113d9cc45dff9e0274c401e42f8d144b56bee2b0
Mike Timmerman <timmermanmike@hotmail.com>
2022-07-25 11:34:15 +0200

    implement sensor class

--- include/sensor.h
+++ include/sensor.h
@@ -21,4 +21,81 @@ class sensor
          */
         sensor( );
 
+
+
+    //
+    // PROTECTED MEMBER FUNCTIONS
+    //
+    protected:
+        /**
+         * @brief Process system output vector
+         * 
+         */
+        void processOutput( VectorXf& _y );
+
+
+
+    //
+    // PROTECTED DATA MEMBERS
+    //
+    protected:
+        VectorXf omega;             // Angular velocity vector
+        VectorXf EulerAngles;       // Absolute orientation (Euler Angles)
+        VectorXf Quaternion;        // Absolute orientation (Quaternion)
+        VectorXf AccelVector;       // Acceleration vector (gravity + linear motion)
+        VectorXf LinAccelVector;    // Linear acceleration vector
+        VectorXf GravityVec;        // Gravity vector'
+        
+        VectorXf PositionVec;       // Position vector
+};
+
+
+class IMUsensor : sensor
+{
+    //
+    // PUBLIC MEMBER FUNCTIONS:
+    //
+    public:
+        /** 
+         * @brief Default constructor
+         */
+        IMUsensor( );
+        
+
+        /**
+         * @brief 
+         * 
+         */
+        VectorXf EulerAngles();
+
+        /**
+         * @brief 
+         * 
+         */
+        VectorXf Quaternion();
+
+        /**
+         * @brief 
+         * 
+         */
+        VectorXf AngularVel();
+
+        /**
+         * @brief 
+         * 
+         */
+        VectorXf Acceleration();
+
+        /**
+         * @brief 
+         * 
+         */
+        VectorXf LinAcceleration();
+
+        /**
+         * @brief 
+         * 
+         */
+        VectorXf GravityVec();
+
 };
---
+++ src/sensor.cpp
@@ -0,0 +1,23 @@
+/**
+ *	\file src/sensor.cpp
+ *	\author Mike Timmerman
+ *	\version 1.0
+ *	\date 2022
+ */
+
+#include "../header.h"    // #include header
+
+
+//
+// PUBLIC MEMBER FUNCTIONS:
+//
+
+sensor::sensor(  ) {}
+
+
+
+
+IMUsensor::IMUsensor(  ) : sensor(  ) {}
+
+
+
