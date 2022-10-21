530e1a90840916ab3499455cbab6762c5082163f
Mike Timmerman <timmermanmike@hotmail.com>
2022-10-21 10:32:24 +0200

    Implement saturation class

--- include/actuator.h
+++ include/actuator.h
@@ -10,7 +10,7 @@
 #include <Eigen/Dense>              // #include module
 using namespace Eigen;              // using namespace of module
 
-class actuator
+class actuator : public saturator
 {
     //
     // PUBLIC MEMBER FUNCTIONS:
@@ -43,67 +43,6 @@ class actuator
         ~actuator(  );
 
 
-        /**
-         * @brief Assign new lower limit on control signals
-         * 
-         * @param[in] _lowerLimits      New lower limit on control signals
-         */
-        void setLowerControlLimit( const VectorXf& _lowerLimits );
-        
-        /**
-         * @brief Assign new upper limit on control signals
-         * 
-         * @param[in] _upperLimits      New upper limit on control signals
-         */
-        void setUpperControlLimit( const VectorXf& _upperLimits );
-
-        /**
-         * @brief Assign new lower limit on certain control signal
-         * 
-         * @param[in] _idx              Index of control signal
-         * @param[in] _lowerLimit       New lower limit on control signals
-         */
-        void setLowerControlLimit( int idx, const float _lowerLimit );
-        
-        /**
-         * @brief Assign new upper limit on control signal
-         * 
-         * @param[in] _idx              Index of control signal
-         * @param[in] _upperLimit       New upper limit on control signals
-         */
-        void setUpperControlLimit( int idx, const float _upperLimit );
-
-
-        /**
-         * @brief Assign new lower rate limit on control signals
-         * 
-         * @param[in] _lowerRateLimits      New lower rate limit on control signals
-         */
-        void setLowerRateLimit( const VectorXf& _lowerRateLimits );
-        
-        /**
-         * @brief Assign new upper rate limit on control signals
-         * 
-         * @param[in] _upperRateLimits      New upper rate limit on control signals
-         */
-        void setUpperRateLimit( const VectorXf& _upperRateLimits );
-
-        /**
-         * @brief Assign new lower rate limit on certain control signal
-         * 
-         * @param[in] _idx                  Index of control signal
-         * @param[in] _lowerRateLimit       New lower rate limit on control signals
-         */
-        void setLowerRateLimit( int idx, const float _lowerRateLimit );
-        
-        /**
-         * @brief Assign new upper rate limit on control signal
-         * 
-         * @param[in] _idx                  Index of control signal
-         * @param[in] _upperRateLimit       New upper rate limit on control signals
-         */
-        void setUpperRateLimit( int idx, const float _upperRateLimit );
-
 
         /**
          * @brief Adjust control signals to actuator dynamics
@@ -128,13 +67,4 @@ class actuator
     private:
         int nu;                             // Number of actuator channels
         float samplingTime;                 // Sampling time
-
-        VectorXf lastU;                     // Previes control signals
-
-        VectorXf lowerLimits;				// Lower limits on control signals
-		VectorXf upperLimits;				// Upper limits on control signals
-
-		VectorXf lowerRateLimits;			// Lower rate limits on control signals
-		VectorXf upperRateLimits;			// Upper rate limits on control signals
-
 };
--- src/actuator.cpp
+++ src/actuator.cpp
@@ -12,19 +12,14 @@
 // PUBLIC MEMBER FUNCTIONS:
 //
 
-actuator::actuator(  ) {}
+actuator::actuator(  ) : saturator(  ) {}
 
 
-actuator::actuator( int _nu, VectorXf _initControl, float _samplingTime  )
+actuator::actuator( int _nu, VectorXf _initControl, float _samplingTime  ) : saturator( _nu, _samplingTime )
 {
-    lowerLimits = VectorXf::Ones( _nu )*-1000000;
-    upperLimits = VectorXf::Ones( _nu )*1000000;
-
-    lowerRateLimits = VectorXf::Ones( _nu )*-1000000;
-    upperRateLimits = VectorXf::Ones( _nu )*1000000;
-
     nu = _nu;
     samplingTime = _samplingTime;
+    
     if ( _initControl.size() > _nu )
         throw std::invalid_argument("Incorrect number of initial control inputs given");
     else if ( _initControl.size() == 0 )
@@ -34,7 +29,7 @@ actuator::actuator( int _nu, VectorXf _initControl, float _samplingTime  )
 }
 
 
-actuator::actuator( const actuator& rhs )
+actuator::actuator( const actuator& rhs ) : saturator( rhs )
 {
     nu = rhs.nu;
     samplingTime = rhs.samplingTime;
@@ -52,111 +47,16 @@ actuator::actuator( const actuator& rhs )
 actuator::~actuator(  ) {}
 
 
-void actuator::setLowerControlLimit( const VectorXf& _lowerLimits  )
-{
-    if ( _lowerLimits.size() != nu ) 
-        throw std::invalid_argument("Incorrect number of lower control limits given");
-    else
-        lowerLimits = _lowerLimits;
-}
-
-void actuator::setUpperControlLimit( const VectorXf& _upperLimits )
-{
-    if ( _upperLimits.size() != nu )
-        throw std::invalid_argument("Incorrect number of upper control limits given");
-    else
-        upperLimits = _upperLimits;
-}
-
-void actuator::setLowerControlLimit( int idx, const float _lowerLimit )
-{
-    if ( idx >= nu )
-        throw std::invalid_argument("Invalid index for control signal given");
-    else if (idx < 0)
-        lowerLimits = VectorXf::Ones(nu)*_lowerLimit;
-    else
-        lowerLimits(idx) = _lowerLimit;
-}
-
-void actuator::setUpperControlLimit( int idx, const float _upperLimit )
-{
-    if ( idx >= nu )
-        throw std::invalid_argument("Invalid index for control signal given");
-    else if (idx < 0)
-        upperLimits = VectorXf::Ones(nu)*_upperLimit;
-    else
-        upperLimits(idx) = _upperLimit;
-}
-
-
-void actuator::setLowerRateLimit( const VectorXf& _lowerRateLimits )
-{
-    if ( _lowerRateLimits.size() != nu ) 
-        throw std::invalid_argument("Incorrect number of lower rate limits given");
-    else
-        lowerRateLimits = _lowerRateLimits;
-}
-
-void actuator::setUpperRateLimit( const VectorXf& _upperRateLimits )
-{
-    if ( _upperRateLimits.size() != nu )
-        throw std::invalid_argument("Incorrect number of upper rate limits given");
-    else
-        upperRateLimits = _upperRateLimits;
-}
-
-void actuator::setLowerRateLimit( int idx, const float _lowerRateLimit )
-{
-    if ( idx >= nu )
-        throw std::invalid_argument("Invalid index for control signal given");
-    else if (idx < 0)
-        lowerRateLimits = VectorXf::Ones(nu)*_lowerRateLimit;
-    else
-        lowerRateLimits(idx) = _lowerRateLimit;
-}
-
-void actuator::setUpperRateLimit( int idx, const float _upperRateLimit )
-{
-    if ( idx >= nu )
-        throw std::invalid_argument("Invalid index for control signal given");
-    else if (idx < 0)
-        upperRateLimits = VectorXf::Ones(nu)*_upperRateLimit;
-    else
-        upperRateLimits(idx) = _upperRateLimit;
-}
-
-
-
 
 void actuator::actuate( VectorXf& _u )
 {
-    unsigned int i;
+    // Saturate control input
+    saturate( _u );
 
-    // Consitency check
-    if ( _u.size() != nu )
-        throw std::invalid_argument("Incorrect number of control signals given");
-    
-    // Set upper and lower bounds
-    VectorXf Uub( upperLimits );
-    VectorXf Ulb( lowerLimits );
-
-    for ( i=0; i<nu; ++i )
-    {
-        if ( lastU(i) + samplingTime*lowerRateLimits(i) > Ulb(i) )
-            Ulb(i) = lastU(i) + samplingTime*lowerRateLimits(i);
-        if ( lastU(i) + samplingTime*upperRateLimits(i) < Uub(i) )
-            Uub(i) = lastU(i) + samplingTime*upperRateLimits(i);
-    }
-
-    // Update control input
-    for ( i=0; i<nu; ++i )
-    {
-        if ( _u(i) > Uub(i) )
-            _u(i) = Uub(i);
-        if ( _u(i) < Ulb(i) )
-            _u(i) = Ulb(i);
-    }
+    // Compute control input rate
     controlRate = (_u - lastU) / samplingTime;
+
+    // Save last control output
     lastU = _u;
 }
 
